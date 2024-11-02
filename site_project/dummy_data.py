e69019d1ea81df158e87dc561a20715ae391633c
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2024-11-02 17:10:52 +0200

    CTP-29 Write basic routing

---
+++ site_project/dummy_data.py
@@ -0,0 +1,23 @@
+products = [
+    {
+        "name": "Laptop",
+        "description":"blao bal blhagljd",
+        "price": 124
+    },
+    {
+        "name": "Smartphone",
+        "description":"blao bal blhagljd",
+        "price": 1546
+    }
+]
+
+cart_products = [
+    {
+        "name": "Laptop",
+        "price": 124
+    },
+    {
+        "name": "Smartphone",
+        "price": 1546
+    }
+]
